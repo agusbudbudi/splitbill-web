1db507267cc133adca116da6ad376b45bd5f19af
Agus Budiman <agusbudbudi@gmail.com>
2026-02-07 12:44:36 +0700

    fix PWA banner

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,28 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/img/pwa-icon.png",
-        sizes: "192x192",
+        sizes: "192x192", // Chrome requires a 192x192 icon. If actual file is 512, this causes a warning, but is required.
         type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/img/pwa-icon.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    screenshots: [
+      {
+        src: "/img/banner-splitbill.jpg",
+        sizes: "1200x630",
+        type: "image/jpg",
+        form_factor: "wide",
+      },
+      {
+        src: "/img/banner-splitbill.jpg",
+        sizes: "1200x630",
+        type: "image/jpg",
       },
     ],
   };

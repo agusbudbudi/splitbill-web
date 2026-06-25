9f990b6a151068974388cdc2a2aec303ad168596
Agus Budiman <agus.budiman@diricare.com>
2026-06-26 00:15:39 +0700

    refactor: remove redundant authentication state logic from AIScanEncourageBanner

--- src/components/home/AIScanEncourageBanner.tsx
+++ src/components/home/AIScanEncourageBanner.tsx
@@ -1,18 +1,11 @@
 "use client";
 
 import React from "react";
-import { useAuthStore } from "@/lib/stores/authStore";
 import { AIScanQuotaBanner } from "@/components/ui/AIScanQuotaBanner";
 
 export const AIScanEncourageBanner = () => {
-  const { user } = useAuthStore();
-  const isSubscribed = user?.subscriptionStatus === "active";
-  const freeScanCount = user?.freeScanCount ?? 5;
-
   return (
     <AIScanQuotaBanner 
-      freeScanCount={freeScanCount} 
-      isSubscribed={isSubscribed}
       showRedirect={true} 
     />
   );
